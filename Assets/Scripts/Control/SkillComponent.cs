1c28b28c0bccc77443e13eb50162d49e6b8d7f41
Xavier <liarchms@outlook.com>
2024-12-31 15:57:34 +0800

    add odin

--- Assets/Scripts/Control/SkillComponent.cs
+++ Assets/Scripts/Control/SkillComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Newtonsoft.Json;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 [Serializable]
