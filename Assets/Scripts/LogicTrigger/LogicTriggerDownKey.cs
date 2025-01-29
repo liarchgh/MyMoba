c29de014cd796735bd958cb05d2d1ac221f5ff18
XavierAs <liarchms@outlook.com>
2025-01-29 22:28:00 +0800

    right mouse key down to keep mvoing

---
+++ Assets/Scripts/LogicTrigger/LogicTriggerDownKey.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class LogicTriggerDownKey: LogicTriggerBase
+{
+    public KeyCode Key;
+    public override bool CheckSkillTrigger()
+    {
+        return Input.GetKeyDown(Key);
+    }
+}
